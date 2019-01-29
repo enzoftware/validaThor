fae9e90a481712d3c53207a16b9294d0c52b5a96
enzoftware <lizama.enzo@gmail.com>
2019-01-28 23:48:00 -0500

    Working on example app

--- app/src/main/java/com/enzoftware/validathorappexample/MainActivity.kt
+++ app/src/main/java/com/enzoftware/validathorappexample/MainActivity.kt
@@ -2,10 +2,16 @@ package com.enzoftware.validathorappexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.enzoftware.validathor.ValidaThor
 
 class MainActivity : AppCompatActivity() {
 
+    // Declare validathor instance
+    private lateinit var validathor : ValidaThor
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Initialize validathor in the app
+        validathor = ValidaThor()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
