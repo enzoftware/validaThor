b8cbb5e44432c56bc6bb5727ce048515cb665f66
enzoftware <lizama.enzo@gmail.com>
2019-01-29 23:15:24 -0500

    Finish the example app for validathor library :zap:

--- app/src/main/java/com/enzoftware/validathorappexample/MainActivity.kt
+++ app/src/main/java/com/enzoftware/validathorappexample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.enzoftware.validathorappexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.enzoftware.validathor.ValidaThor
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -27,14 +28,41 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun validateEmailField(s: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (s.isEmpty()) {
+            edt_email.error = "Field is empty!"
+        }
+
+        if (!validathor.isEmail(s)) {
+            edt_email.error = "Invalid Email"
+        } else {
+            Toast.makeText(this, "Valid Email!", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun validatePasswordField(s: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (s.isEmpty()) {
+            edt_password.error = "Field is empty!"
+        }
+
+        if (validathor.isAtLeastLength(s, 8)
+            && validathor.hasAtLeastOneDigit(s)
+            && validathor.hasAtLeastOneUpperCaseCharacter(s)
+            && validathor.hasAtLeastOneSpecialCharacter(s)) {
+            Toast.makeText(this, "Valid Password!", Toast.LENGTH_SHORT).show()
+        } else {
+            edt_password.error = "Password needs to be a minimum length of 8 characters and should have at least 1 digit, 1 upppercase letter and 1 special character "
+        }
     }
 
     private fun validateCreditCardNumberField(s: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (s.isEmpty()) {
+            edt_creditcard.error = "Field is empty!"
+        }
+
+        if (!validathor.validateCreditCard(s)) {
+            edt_creditcard.error = "Invalid Credit Card number!"
+        } else {
+            Toast.makeText(this, "Valid Credit Card Number!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
