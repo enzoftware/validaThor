917eb56ddd5940c78b432f11a9d0b160905983c8
enzoftware <lizama.enzo@gmail.com>
2019-01-29 23:03:23 -0500

    Layout of example app done

--- app/src/main/java/com/enzoftware/validathorappexample/MainActivity.kt
+++ app/src/main/java/com/enzoftware/validathorappexample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.enzoftware.validathorappexample
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.enzoftware.validathor.ValidaThor
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,5 +15,26 @@ class MainActivity : AppCompatActivity() {
         validathor = ValidaThor()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupUiWidgets()
+    }
+
+    private fun setupUiWidgets() {
+        btn_validate.setOnClickListener {
+            validateEmailField(edt_email.text.toString())
+            validatePasswordField(edt_password.text.toString())
+            validateCreditCardNumberField(edt_creditcard.text.toString())
+        }
+    }
+
+    private fun validateEmailField(s: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun validatePasswordField(s: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun validateCreditCardNumberField(s: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
---
+++ app/src/main/java/com/enzoftware/validathorappexample/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.enzoftware.validathorappexample
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_splash)
+
+        val handler = Handler()
+        handler.postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }, 2000)
+    }
+}
